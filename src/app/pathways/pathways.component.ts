2e55b0f86bddc225f8ac08657c0dfcc9da6c6618
gavinfarrell <gavinfarrell97@gmail.com>
2025-09-28 11:56:50 +0100

    Fixes expansion box arrows

--- src/app/pathways/pathways.component.ts
+++ src/app/pathways/pathways.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pathways',
   templateUrl: './pathways.component.html',
-  styleUrls: ['./pathways.component.scss']
 })
 export class PathwaysComponent implements OnInit {
+
+  openCards: { [key: number]: boolean } = {};
 
   constructor() { }
 
   ngOnInit(): void {
+    // Initialize all cards to be closed
+    for (let i = 1; i <= 9; i++) {
+      this.openCards[i] = false;
+    }
   }
 
+  toggleCard(cardId: number): void {
+    this.openCards[cardId] = !this.openCards[cardId];
+  }
+
+  isCardOpen(cardId: number): boolean {
+    return this.openCards[cardId];
+  }
 }
